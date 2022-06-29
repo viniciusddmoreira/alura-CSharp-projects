0fef292dd6c11e3cd57bdd43333f591d81180d2f
viniciusddmoreira <viniciusdanielmoreira@gmail.com>
2022-06-28 21:42:24 -0300

    Add project files

---
+++ ByteBank/ByteBank/Program.cs
@@ -0,0 +1,30 @@
+﻿using ByteBank;
+using ByteBank.Funcionarios;
+
+GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+Funcionario vinicius = new Funcionario();
+vinicius.Nome = "Vinicius";
+vinicius.CPF = "123.456.789-00";
+vinicius.Salario = 3000;
+
+
+
+Console.WriteLine(vinicius.Nome);
+Console.WriteLine(vinicius.Salario);
+
+Diretor roberta = new Diretor();
+roberta.Nome = "Roberta";
+roberta.CPF = "555.444.666-22";
+roberta.Salario = 7000;
+
+Console.WriteLine(roberta.Nome);
+Console.WriteLine(roberta.Salario);
+
+gerenciador.Registrar(vinicius);
+gerenciador.Registrar(roberta);
+
+Console.WriteLine("Total de bonificações:" + gerenciador.GetTotalBonificacao());
+
+
+
