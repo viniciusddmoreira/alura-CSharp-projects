9e5b00670144f6afe35bd43ac712c65640330ed7
viniciusddmoreira <viniciusdanielmoreira@gmail.com>
2022-06-29 22:05:20 -0300

    Add project files

---
+++ ByteBankExceptions/ByteBank/SaldoInsuficienteException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class SaldoInsuficienteException : OperacaoFinanceiraException
+    {
+        public double Saldo { get; }
+        public double ValorSaque { get; }
+        public SaldoInsuficienteException()
+        {
+        }
+
+        public SaldoInsuficienteException(double saldo, double valorSaque)
+            : this("Tentativa de saque de " + valorSaque + " com saldo de " + saldo)
+        {
+            Saldo = saldo;
+            ValorSaque = valorSaque;
+        }
+
+        public SaldoInsuficienteException(string mensagem)
+            : base(mensagem)
+        {
+        }
+        public SaldoInsuficienteException(string mensagem, Exception excecaoInterna)
+            : base(mensagem, excecaoInterna)
+        {
+
+        }
+    }
+}
